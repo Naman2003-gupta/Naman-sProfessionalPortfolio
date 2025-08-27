a4d34cccce581f37ed3b7e393be5ac3b0ddf3a08
naman <naman05mah@gmail.com>
2025-08-27 16:39:36 +0530

    Naman

---
+++ src/constants.js
@@ -0,0 +1,292 @@
+// Skills Section Logo's
+import htmlLogo from './assets/tech_logo/html.png';
+import cssLogo from './assets/tech_logo/css.png';
+import sassLogo from './assets/tech_logo/sass.png';
+import javascriptLogo from './assets/tech_logo/javascript.png';
+import reactjsLogo from './assets/tech_logo/reactjs.png';
+import angularLogo from './assets/tech_logo/angular.png';
+import reduxLogo from './assets/tech_logo/redux.png';
+import nextjsLogo from './assets/tech_logo/nextjs.png';
+import tailwindcssLogo from './assets/tech_logo/tailwindcss.png';
+import gsapLogo from './assets/tech_logo/gsap.png';
+import materialuiLogo from './assets/tech_logo/materialui.png';
+import bootstrapLogo from './assets/tech_logo/bootstrap.png';
+import springbootLogo from './assets/tech_logo/springboot.png';
+import nodejsLogo from './assets/tech_logo/nodejs.png';
+import expressjsLogo from './assets/tech_logo/express.png';
+import mysqlLogo from './assets/tech_logo/mysql.png';
+import mongodbLogo from './assets/tech_logo/mongodb.png';
+import firebaseLogo from './assets/tech_logo/firebase.png';
+import cLogo from './assets/tech_logo/c.png';
+import cppLogo from './assets/tech_logo/cpp.png';
+import javaLogo from './assets/tech_logo/java.png';
+import pythonLogo from './assets/tech_logo/python.png';
+import typescriptLogo from './assets/tech_logo/typescript.png';
+import gitLogo from './assets/tech_logo/git.png';
+import githubLogo from './assets/tech_logo/github.png';
+import vscodeLogo from './assets/tech_logo/vscode.png';
+import postmanLogo from './assets/tech_logo/postman.png';
+import mcLogo from './assets/tech_logo/mc.png';
+import figmaLogo from './assets/tech_logo/figma.png';
+import netlifyLogo from './assets/tech_logo/netlify.png';
+import vercelLogo from './assets/tech_logo/vercel.png';
+import postgreLogo from './assets/tech_logo/postgre.png';
+import csharpLogo from './assets/tech_logo/csharp.png';
+
+// Experience Section Logo's
+import webverseLogo from './assets/company_logo/BharatSkillz.png';
+import agcLogo from './assets/company_logo/TeachNook.png';
+import newtonschoolLogo from './assets/company_logo/Sparks.jpeg';
+
+// Education Section Logo's
+import glaLogo from './assets/education_logo/LPU.png';
+import bsaLogo from './assets/education_logo/DAV.jpeg';
+import vpsLogo from './assets/education_logo/Hilltop.jpeg';
+
+// Project Section Logo's
+import githubdetLogo from './assets/work_logo/Chat.png';
+import csprepLogo from './assets/work_logo/cs_prep.png';
+import movierecLogo from './assets/work_logo/NAMANCORPORATIONBankk.png';
+import taskremLogo from './assets/work_logo/AI-Resume .png';
+import npmLogo from './assets/work_logo/npm.png';
+import webverLogo from './assets/work_logo/bonvoyage.png';
+import cmLogo from './assets/work_logo/cm.png';
+import imagesearchLogo from './assets/work_logo/Ecommerce.png';
+import removebgLogo from './assets/work_logo/gemini.jpg';
+
+
+export const SkillsInfo = [
+  {
+    title: 'Frontend',
+    skills: [
+      { name: 'HTML', logo: htmlLogo },
+      { name: 'CSS', logo: cssLogo },
+      { name: 'SASS', logo: sassLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'React JS', logo: reactjsLogo },
+      { name: 'Angular', logo: angularLogo },
+      { name: 'Redux', logo: reduxLogo },
+      { name: 'Next JS', logo: nextjsLogo },
+      { name: 'Tailwind CSS', logo: tailwindcssLogo },
+      { name: 'GSAP', logo: gsapLogo },
+      { name: 'Material UI', logo: materialuiLogo },
+      { name: 'Bootstrap', logo: bootstrapLogo },
+    ],
+  },
+  {
+    title: 'Backend',
+    skills: [
+      { name: 'Springboot', logo: springbootLogo },
+      { name: 'Node JS', logo: nodejsLogo },
+      { name: 'Express JS', logo: expressjsLogo },
+      { name: 'MySQL', logo: mysqlLogo },
+      { name: 'MongoDB', logo: mongodbLogo },
+      { name: 'Firebase', logo: firebaseLogo },
+      { name: 'PostgreSQL', logo: postgreLogo },
+    ],
+  },
+  {
+    title: 'Languages',
+    skills: [
+      { name: 'C', logo: cLogo },
+      { name: 'C++', logo: cppLogo },
+      { name: 'Java', logo: javaLogo },
+      { name: 'Python', logo: pythonLogo },
+      { name: 'C-Sharp', logo: csharpLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'TypeScript', logo: typescriptLogo },
+    ],
+  },
+  {
+    title: 'Tools',
+    skills: [
+      { name: 'Git', logo: gitLogo },
+      { name: 'GitHub', logo: githubLogo },
+      { name: 'VS Code', logo: vscodeLogo },
+      { name: 'Postman', logo: postmanLogo },
+      { name: 'Compass', logo: mcLogo },
+      { name: 'Vercel', logo: vercelLogo },
+      { name: 'Netlify', logo: netlifyLogo },
+      { name: 'Figma', logo: figmaLogo },
+    ],
+  },
+];
+
+  export const experiences = [
+    {
+      id: 0,
+      img: webverseLogo,
+      role: "Business Analyst",
+      company: "BharatSkillz",
+      date: "April 2025 - Present",
+      desc: "Bridged business strategy with technology by developing scalable web applications using MongoDB, Express, React, and Node.js. Delivered responsive UIs, RESTful APIs, and optimized workflows while collaborating with cross-functional teams in agile environments. Focused on data-driven insights to align business goals with technical solutions.",
+      skills: [
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "React JS",
+        "TypeScript",
+        "Node JS",
+        "Tailwind CSS",
+        "MongoDb",
+        "Redux",
+        " Next Js",
+      ],
+    },
+    {
+      id: 1,
+      img: agcLogo,
+      role: "Software Engineer Intern",
+      company: "TeachNook",
+      date: "Jan 2025 - Apr 2025",
+      desc: "Contributed to innovative projects as a Fullstack Engineer, leading both frontend and backend development using technologies such as HTML, CSS, JavaScript, PHP, SQL, Bootstrap, and ReactJS. Worked closely with the team to deliver responsive, high-performance web applications and improve user experience through seamless integration of various technologies.",
+      skills: [
+        "ReactJS",
+        "Redux",
+        "JavaScript",
+        "Tailwind CSS",
+        "HTML",
+        "CSS",
+        "SQL",
+      ],
+    },
+    {
+      id: 2,
+      img: newtonschoolLogo,
+      role: "Web Developer ",
+      company: "Sparks Foundation Singapore",
+      date: "September 2021 - August 2022",
+      desc: "Worked as a Frontend Developer Intern, designing and implementing scalable UI components and responsive websites using HTML, CSS, JavaScript, Bootstrap, and Material UI. Collaborated with the design team to translate wireframes and prototypes from Figma into interactive, user-friendly web pages.",
+      skills: [
+        "HTML",
+        "CSS",
+        "Javascript",
+        "Bootstrap",
+        "Figma",
+        "Material UI",
+      ],
+    },
+  ];
+  
+  export const education = [
+    {
+      id: 0,
+      img: glaLogo,
+      school: "Lovely Professional University, Jalandhar",
+      date: "Sept 2021 - July 2025",
+      grade: "7.81 CGPA",
+      desc: "I have completed my Bachelor's degree (B.Tech) in Computer Science And Engineering from LPU University, Jalandhar. During my time at LPU, I gained a strong foundation in programming, software development, and computer science principles. I have studied courses such as Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Web Development, and Software Engineering. I actively participated in various workshops and technical events, which enhanced my skills and knowledge. My experience at LPU University has been instrumental in shaping my technical abilities and professional growth.",
+      degree: "B.Tech CSE(Hons)Full Stack Software Development - B.Tech CSE(Hons)",
+    },
+    {
+      id: 1,
+      img: bsaLogo,
+      school: "DAV College, Banikhet",
+      date: "Sept 2020 - Aug 2021",
+      grade: "87.2%",
+     desc: "I completed my class 12 education from DAV College Banikhet, , under the HPBOSE board, where I studied Physics, Chemistry, and Mathematics (PCM) with Computer Science.",
+      degree: "CBSE(X) - PCM with Computer Science",
+    },
+    {
+      id: 2,
+      img: vpsLogo,
+      school: "Dalhousie Hilltop School, Dalhousie",
+      date: "Apr 2018 - March 2019",
+      grade: "78%",
+      desc: "I completed my class 10 education from Dalhousie Hilltop School, , under the CBSE board, where I studied Physics, Chemistry, and Mathematics (PCM) with Computer Science.",
+      degree: "CBSE(X)",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 0,
+      title: "Realtime Chatting Application",
+      description:
+        "A real-time chat application built using Spring Boot and WebSocket, designed to enable seamless instant messaging between users. It supports multiple chat rooms, live message broadcasting, and user-friendly interactions. The backend ensures reliable communication with WebSocket protocol, while the frontend provides a clean and responsive interface for smooth chatting experience.",
+      image: githubdetLogo,
+      tags: ["SpringBoot","HTML", "CSS", "JavaScript","Kafka" ,"API"],
+      github: "https://github.com/Naman2003-gupta/ChatApplication-with-SpringBoot",
+      webapp: "https://app.netlify.com/projects/springdgchat//",
+    },
+    {
+      id: 1,
+      title: "BrainQuest",
+      description:
+        "A full-stack quiz-based platform designed for GATE/UGC NET students to practice previous year questions and create customized tests. The platform offers comprehensive profile stats and detailed results, helping students track and improve their preparation journey effectively.",
+      image: csprepLogo,
+      tags: ["React JS", "Node.js", "MongoDB", "Express", "HTML", "CSS", "JavaScript"],
+      github: "https://github.com/Naman2003-gupta/BrainQuest",
+      webapp: "https://brainquestt.netlify.app//",
+    },
+    {
+      id: 2,
+      title: "Online Banking System",
+      description:
+        "A secure and user-friendly web-based banking application designed to simplify everyday financial transactions. The system was developed using HTML, CSS, PHP, and JavaScript, focusing on both functionality and an intuitive interface.",
+      image: movierecLogo,
+      tags: ["React JS", "API", "HTML", "CSS", "JavaScript"],
+      github: "https://github.com/Naman2003-gupta/Online-Banking-System-By-Naman-Gupta",
+      webapp: "https://namancorporationbank.netlify.app/#/",
+    },
+    {
+      id: 3,
+      title: "Github Link Finder Using API",
+      description:
+        "A simple yet powerful tool that allows users to quickly find and extract GitHub profile links using the GitHub API. By entering a username or query, the application fetches accurate profile details and provides direct access to repositories, followers, and other related links. This project demonstrates API integration, JSON data handling, and a clean user-friendly interface.",
+      image: npmLogo,
+      tags: ["React JS", "Node.js", "NPM", "Validation"],
+      github: "https://github.com/Naman2003-gupta/GithubFinder",
+      webapp: "https://githublinkfinder.netlify.app/",
+    },
+    {
+      id: 4,
+      title: "AI Resume Builder Chrome Extension Tool",
+      description:
+        "A lightweight Chrome Extension that automates email extraction from web pages, streamlining recruiter outreach and lead generation.",
+      image: taskremLogo,
+      tags: ["JavaScript", "Chrome Extension", "HTML", "CSS"],
+      github: "https://github.com/codingmastr/Task-Reminder-Tool",
+      webapp: "chrome://extensions/?id=kngheeibjnnidhfoomkpnbeghackheci",
+    },
+    {
+      id: 5,
+      title: "BonVoyage",
+      description:
+        "Bon Voyage is a tourism project built with the MERN stack, offering eco-friendly stays, tour packages, and community trips. It features a modern React-based UI, secure booking with Node.js and Express, and efficient data management using MongoDB to deliver a seamless travel experience",
+      image: webverLogo,
+      tags: ["HTML", "CSS", "JavaScript", "MongoDB", "Express", "React JS", "Node.js"],
+      github: "https://github.com/codingmastr/Webverse-Digital",
+      webapp: "https://bonvoyage.com/",
+    },
+    {
+      id: 6,
+      title: "Coding Master",
+      description:
+        "An ed-tech platform where users can access tech and coding-related blogs, notes, interview questions, e-books, and premium content with payment integration. Built with full-stack technologies for a seamless learning experience.",
+      image: cmLogo,
+      tags: ["React JS", "Node.js", "MongoDB", "Express", "Payment Integration"],
+      github: "https://codingmasterweb.in/",
+      webapp: "https://codingmasterweb.in/",
+    },
+    {
+      id: 7,
+      title: "Ecommerce Website",
+      description:
+        "A fully functional online shopping platform built to provide a seamless buying experience. The website includes features like product listings with search and filter options, shopping cart, secure checkout, order tracking, and user authentication. It is designed with a responsive and modern UI, ensuring smooth performance across devices.",
+      image: imagesearchLogo,
+      tags: ["React JS", "API", "Search Feature", "CSS", "Javascript"],
+      github: "https://github.com/Naman2003-gupta/EcommerceWebsite/",
+      webapp: "https://ecommercebynaman.netlify.app//",
+    },
+    {
+      id: 8,
+      title: "Gemini API",
+      description:
+        "Developed a real-time voice-to-text application using the Gemini API, enabling seamless speech recognition and instant transcription. Integrated API key authentication for secure access and optimized for fast, accurate voice processing.",
+      image: removebgLogo,
+      tags: ["React JS", "API", "RealTime Audio LIstener API","Google Gemini API ","HTML", "CSS", "Javascript"],
+      github: "https://github.com/codingmastr/Image-Background-Remover",
+      webapp: "https://removeyourbg.netlify.app/",
+    },
+  ];  
